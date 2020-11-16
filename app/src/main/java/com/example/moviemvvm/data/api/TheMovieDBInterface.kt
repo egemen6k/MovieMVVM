b8c245ad6497e2beaefb549eac8b1910778a19d3
egemen6k <egemen.altikardesler@gmail.com>
2020-11-16 20:28:08 +0300

    +MainActivity xml recyclerview added

--- app/src/main/java/com/example/moviemvvm/data/api/TheMovieDBInterface.kt
+++ app/src/main/java/com/example/moviemvvm/data/api/TheMovieDBInterface.kt
@@ -1,9 +1,11 @@
 package com.example.moviemvvm.data.api
 
 import com.example.moviemvvm.data.vo.MovieDetails
+import com.example.moviemvvm.data.vo.MovieResponse
 import io.reactivex.Single
 import retrofit2.http.GET
 import retrofit2.http.Path
+import retrofit2.http.Query
 
 //reactivex observer-observables
 
@@ -20,4 +22,7 @@ interface TheMovieDBInterface {
     //dönüş değeri Single oluyor
     @GET("movie/{movie_id}")
     fun getMovieDetails(@Path("movie_id")id: Int) : Single<MovieDetails>
+
+    @GET("movie/popular")
+    fun getPopularMovie(@Query("page") page:Int) : Single<MovieResponse>
 }
