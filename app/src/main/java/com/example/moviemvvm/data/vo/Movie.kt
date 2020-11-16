f7714d54bf8786e34b5ce317317d2e836c079d1e
egemen6k <egemen.altikardesler@gmail.com>
2020-11-16 20:23:22 +0300

    +MovieList data class created

---
+++ app/src/main/java/com/example/moviemvvm/data/vo/Movie.kt
@@ -0,0 +1,16 @@
+package com.example.moviemvvm.data.vo
+
+
+import com.google.gson.annotations.SerializedName
+
+data class Movie(
+    @SerializedName("id")
+    val id: Int,
+    @SerializedName("poster_path")
+    val posterPath: String,
+    @SerializedName("release_date")
+    val releaseDate: String,
+    @SerializedName("title")
+    val title: String
+
+)
